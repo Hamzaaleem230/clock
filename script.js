3e423e71ba3879399b4eac4282408ef4001698e5
Syed Hamza Aleem <162742428+Hamzaaleem230@users.noreply.github.com>
2025-11-23 13:30:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const hr = document.getElementById("hr");
+const mn = document.getElementById("mn");
+const sc = document.getElementById("sc");
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hr.style.transform = `rotate(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotate(${mm}deg)`;
+  sc.style.transform = `rotate(${ss}deg)`;
+});
